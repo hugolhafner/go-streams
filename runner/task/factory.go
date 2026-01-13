8af3f65f7640b9214701e073597a8c23015c2b7f
Hugo Hafner <hello@hugohafner.com>
2026-01-13 15:56:49 +0000

    feat(app): init

--- runner/task/factory.go
+++ runner/task/factory.go
@@ -2,8 +2,23 @@ package task
 
 import (
 	"github.com/hugolhafner/go-streams/runner/log"
+	"github.com/hugolhafner/go-streams/topology"
 )
 
 type Factory interface {
 	CreateTask(partition log.TopicPartition, producer log.Producer) (Task, error)
+}
+
+var _ Factory = (*topologyTaskFactory)(nil)
+
+type topologyTaskFactory struct {
+	topology *topology.Topology
+}
+
+func NewTopologyTaskFactory(t *topology.Topology) Factory {
+	return &topologyTaskFactory{topology: t}
+}
+
+func (f *topologyTaskFactory) CreateTask(partition log.TopicPartition, producer log.Producer) (Task, error) {
+	panic("not implemented")
 }
